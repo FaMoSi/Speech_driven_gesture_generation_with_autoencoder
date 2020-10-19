e6130061869a01c011bcc81bc18068319b1a7273
Famosi <simone.faggi@yahoo.it>
2020-10-19 12:33:56 +0200

    inputshape

--- train.py
+++ train.py
@@ -126,7 +126,7 @@ def train_CNN(model_file):
     model.add(Activation('relu'))
     model.add(Dropout(0.1))
     
-    model.add(Dense(N_OUTPUT))
+    model.add(Dense(N_OUTPUT, 1))
     model.add(Activation('linear'))
 
     print(model.summary())
