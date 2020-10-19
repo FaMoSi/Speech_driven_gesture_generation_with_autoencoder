b880d62477e3ca55372fb04cdd24b563538dbbc5
Famosi <simone.faggi@yahoo.it>
2020-10-19 12:46:03 +0200

    inputshape

--- train.py
+++ train.py
@@ -126,7 +126,7 @@ def train_CNN(model_file):
     model.add(Activation('relu'))
     model.add(Dropout(0.1))
     
-    model.add(Dense(N_OUTPUT), output_shape=(N_OUTPUT, 1))
+    model.add(Dense(N_OUTPUT))
     model.add(Activation('linear'))
 
     print(model.summary())
