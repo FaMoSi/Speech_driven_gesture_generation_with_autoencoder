45363a4710544a8605e79b6f0d88d3249968ad13
Famosi <simone.faggi@yahoo.it>
2020-10-19 18:20:56 +0200

    add my ae

---
+++ motion_repr_learning/ae/my_train.py
@@ -0,0 +1,62 @@
+import Keras
+from keras import layers
+from keras.callbacks import TensorBoard
+from keras import backend as K
+
+
+def sampling(args):
+    z_mean, z_log_sigma = args
+    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim),
+                              mean=0., stddev=0.1)
+    return z_mean + K.exp(z_log_sigma) * epsilon
+
+z = layers.Lambda(sampling)([z_mean, z_log_sigma])
+
+original_dim = 45
+intermediate_dim = 40
+latent_dim = 36
+
+inputs = keras.Input(shape=(original_dim,))
+h = layers.Dense(intermediate_dim, activation='relu')(inputs)
+z_mean = layers.Dense(latent_dim)(h)
+z_log_sigma = layers.Dense(latent_dim)(h)
+
+# Create encoder
+encoder = keras.Model(inputs, [z_mean, z_log_sigma, z], name='encoder')
+
+# Create decoder
+latent_inputs = keras.Input(shape=(latent_dim,), name='z_sampling')
+x = layers.Dense(intermediate_dim, activation='relu')(latent_inputs)
+outputs = layers.Dense(original_dim, activation='relu')(x)
+decoder = keras.Model(latent_inputs, outputs, name='decoder')
+
+# instantiate VAE model
+outputs = decoder(encoder(inputs)[2])
+vae = keras.Model(inputs, outputs, name='vae_mlp')
+
+reconstruction_loss = keras.losses.mse(inputs, outputs)
+reconstruction_loss *= original_dim
+kl_loss = 1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma)
+kl_loss = K.sum(kl_loss, axis=-1)
+kl_loss *= -0.5
+vae_loss = K.mean(reconstruction_loss + kl_loss)
+vae.add_loss(vae_loss)
+vae.compile(optimizer='adam')
+
+vae.summary()
+
+# Check parameters
+check_params()
+
+# Get the data
+DATA_DIR = sys.argv[1]
+train_normalized_data, train_data, test_normalized_data, test_data, dev_normalized_data, \
+max_val, mean_pose = prepare_motion_data(DATA_DIR)
+
+vae.fit(train_data, train_data,
+        epochs=20,
+        batch_size=64,
+        validation_data=(test_data, test_data))
+
+vae.save_weights('autoencoder_weights.h5')
+
